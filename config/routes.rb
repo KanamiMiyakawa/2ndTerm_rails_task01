9d2983b1aeee0f7e0d3947ceb7a32afef43eaa4f
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-15 06:22:58 +0000

    implement resources method on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments/index'
-  get 'comments/create'
-  get 'comments/new'
-  get 'comments/edit'
-  get 'comments/show'
-  get 'comments/update'
-  get 'comments/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :comments do
+  end
 end
