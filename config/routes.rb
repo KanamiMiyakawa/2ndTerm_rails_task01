4dc26bdde0d45edc32dc792b850b43da203afd7a
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-15 06:04:26 +0000

    g controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/create'
+  get 'comments/new'
+  get 'comments/edit'
+  get 'comments/show'
+  get 'comments/update'
+  get 'comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
