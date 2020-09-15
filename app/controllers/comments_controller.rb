674a7b48c1e2657477afe194e4e9dfff4e8aea4c
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-15 07:08:13 +0000

    create new action, view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,13 @@ class CommentsController < ApplicationController
   def index
     @comments = Comment.all
   end
-
   def create
+    @comment = Comment.new(content: params[:comment][:content])
+    @comment.save
+    redirect_to comments_path, notice: "コメントを投稿しました！"
   end
-
   def new
+    @comment = Comment.new
   end
 
   def edit
