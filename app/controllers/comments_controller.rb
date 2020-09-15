aa0326795b055db714a5604f3f1834183fca446b
kanami miyakawa <kedamap@yahoo.co.jp>
2020-09-15 09:12:43 +0000

    fix method name

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_blog, only: [:edit,:update,:destroy]
+  before_action :set_comment, only: [:edit,:update,:destroy]
   def index
     @comments = Comment.all
   end
@@ -39,7 +39,7 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:content)
   end
-  def set_blog
+  def set_comment
     @comment = Comment.find(params[:id])
   end
 end
